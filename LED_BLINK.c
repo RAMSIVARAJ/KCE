aa78d42afe21e249fc7222a2447b13f1db174ba1
RAMSIVARAJ <127391355+RAMSIVARAJ@users.noreply.github.com>
2023-03-28 15:10:27 +0530

    Update LED_BLINK.c

--- LED_BLINK.c
+++ LED_BLINK.c
@@ -1,19 +1,21 @@
-#include <AT89S52.h>	/* include header for AT89S52 */ 
+#include <AT89S52.h>
 
-void wait(void);      /* declare wait function */
+void delay(void);
 
 void main()
 {
-  P2_0=0;    	/*Turn ON the LED */
-  while(1)  	/* Super loop (Infinite loop) */
-  {
-  }
+ while(1)
+ {
+  P2_0=0;
+  delay();
+  P2_0=1;
+  delay();
+ }
 }
 
-void wait()
+void delay(void)
 {
-  int i;
-  for(i=0;i<10;i++) /* generate a dummy loop with 10 counts */
-  {
-  }
+ int i;
+ for(i=0;i<100;i++)
+ {}
 }
